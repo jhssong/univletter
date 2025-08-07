896d6b036145f1b778d1bc3d070989c994532108
jhssong <jhssong02@gmail.com>
2025-08-07 23:55:09 +0900

    init

---
+++ src/main/java/com/jhssong/univletter/UnivletterApplication.java
@@ -0,0 +1,13 @@
+package com.jhssong.univletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnivletterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhssong/univletter/UnivletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhssong.univletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
