4dd4637668ea42b06672a48bfc4e1870cf34ce17
jhssong <jhssong02@gmail.com>
2025-08-10 16:25:32 +0900

    feat: Add BaseTimeEntity

--- src/main/java/com/jhssong/univletter/UnivletterApplication.java
+++ src/main/java/com/jhssong/univletter/UnivletterApplication.java
@@ -2,12 +2,14 @@ package com.jhssong.univletter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UnivletterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UnivletterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UnivletterApplication.class, args);
+    }
 
 }
