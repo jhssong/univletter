1cada5996857bded23449fb818b9794f7bb71ae9
jhssong <jhssong02@gmail.com>
2025-08-12 00:08:31 +0900

    fix: Fix validation annotation for LocalDate

--- src/main/java/com/jhssong/univletter/domain/notice/dto/NoticeDTO.java
+++ src/main/java/com/jhssong/univletter/domain/notice/dto/NoticeDTO.java
@@ -1,7 +1,7 @@
 package com.jhssong.univletter.domain.notice.dto;
 
-import com.jhssong.univletter.domain.notice.entity.Notice;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import lombok.Builder;
 
@@ -13,20 +13,11 @@ public record NoticeDTO(
         String link,
         int views,
         String author,
-        @NotBlank(message = "작성일")
+        @NotNull(message = "작성일")
         LocalDate writtenAt,
         @NotBlank(message = "게시판 이름")
         String boardName,
         @NotBlank(message = "게시판 분류")
         String boardSubName
 ) {
-    public static NoticeDTO fromEntity(Notice notice) {
-        return NoticeDTO.builder()
-                .title(notice.getTitle())
-                .link(notice.getLink())
-                .views(notice.getViews())
-                .author(notice.getAuthor())
-                .writtenAt(notice.getWrittenAt())
-                .build();
-    }
 }
