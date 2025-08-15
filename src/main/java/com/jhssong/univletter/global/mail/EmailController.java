4d5de0ad68c2f28304f038a80182d90803c1313e
jhssong <jhssong02@gmail.com>
2025-08-15 16:57:35 +0900

    chore: Change api url for sendDailyNewsletterManually

--- src/main/java/com/jhssong/univletter/global/mail/EmailController.java
+++ src/main/java/com/jhssong/univletter/global/mail/EmailController.java
@@ -2,7 +2,6 @@ package com.jhssong.univletter.global.mail;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,20 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@Profile("local")
-public class EmailTestController {
+public class EmailController {
 
     private final EmailSchedulerService emailSchedulerService;
 
-    @PostMapping("/api/admin/test-send")
+    @PostMapping("/api/admin/send-newsletter-manually")
     public ResponseEntity<String> sendDailyNewsletterManually() {
-        log.info("이메일 전송 테스트 시작.");
+        log.info("이메일 전송 수동 시작.");
         try {
             emailSchedulerService.sendDailyArticle();
-            return ResponseEntity.ok("이메일 전송 테스트 완료.");
+            return ResponseEntity.ok("이메일 전송 완료.");
         } catch (Exception e) {
-            log.error("이메일 전송 테스트 중 오류 발생: {}", e.getMessage(), e);
-            return ResponseEntity.internalServerError().body("이메일 전송 테스트 중 오류 발생: " + e.getMessage());
+            log.error("이메일 전송 중 오류 발생: {}", e.getMessage(), e);
+            return ResponseEntity.internalServerError().body("이메일 전송 중 오류 발생: " + e.getMessage());
         }
     }
 }
