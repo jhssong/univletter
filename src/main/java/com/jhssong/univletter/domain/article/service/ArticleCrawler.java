3e272c629734405104661f5453bfe2f6abd48e86
jhssong <jhssong02@gmail.com>
2025-11-15 01:43:51 +0900

    feat: Add KNU board crawling method

--- src/main/java/com/jhssong/univletter/domain/article/service/ArticleCrawler.java
+++ src/main/java/com/jhssong/univletter/domain/article/service/ArticleCrawler.java
@@ -5,9 +5,11 @@ import com.jhssong.univletter.domain.article.exception.ArticleExceptionUtils;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -23,8 +25,114 @@ public class ArticleCrawler {
 
     public void crawl(int timeWindow) {
         log.info("크롤링 작업 시작");
+        crawlKNU(timeWindow);
         crawlKNUCSE(timeWindow);
         log.info("크롤링 작업 종료");
+    }
+
+    private boolean checkDateAndSaveArticle(int timeWindow, LocalDate writtenAt, String title, String link,
+                                            Integer views, String author, String boardName, String subName) {
+        // 하루 전보다 오래된 글이면 종료
+        LocalDate oneDayAgo = LocalDate.now().minusDays(timeWindow);
+        if (writtenAt.isBefore(oneDayAgo)) {
+            return false;
+        }
+
+        // 최신 글이면 ArticleReqDTO 객체 생성
+        ArticleReqDTO articleReqDTO = ArticleReqDTO.builder()
+                .title(title)
+                .link(link)
+                .views(views)
+                .author(author)
+                .writtenAt(writtenAt)
+                .boardName(boardName)
+                .boardSubName(subName)
+                .build();
+
+        log.debug("분야: {}, 제목: {}, 작성일: {}", subName, title, writtenAt);
+
+        articleService.addNotice(articleReqDTO);
+        return true;
+    }
+
+    private void crawlKNU(int timeWindow) {
+        String boardName = "경북대학교";
+        String baseURL = "https://knu.ac.kr/wbbs/wbbs/bbs/btin/";
+        String subName = "공지사항";
+
+        boolean active = true;
+        int pageCnt = 1;
+
+        while (active) {
+            String url = baseURL + "list.action?bbs_cde=1&pageIndex=" + pageCnt;
+            pageCnt++;
+
+            try {
+                Document doc = Jsoup.connect(url).get();
+                Elements rows = doc.select("tbody tr");
+
+                if (rows.isEmpty()) {
+                    break;
+                }
+
+                for (Element row : rows) {
+                    // 게시글 번호
+                    String number = Optional.ofNullable(row.selectFirst("td.num"))
+                            .map(Element::text).orElse(null);
+
+                    // 번호가 '공지'가 아닌 경우만 크롤링
+                    if (number != null && !StringUtils.isNumeric(number)) {
+                        continue;
+                    }
+
+                    // 제목 및 링크
+                    Element titleLink = row.selectFirst("td.subject a");
+                    String title = titleLink != null ? titleLink.text() : null;
+                    String href = titleLink != null ? titleLink.attr("href") : null;
+                    String link = null;
+                    if (href != null) {
+                        String docNo = Arrays.stream(href.split("&"))
+                                .filter(s -> s.startsWith("btin.doc_no="))
+                                .map(s -> s.split("=")[1])
+                                .findFirst()
+                                .orElse(null);
+                        link = baseURL + "viewBtin.action?btin.bbs_cde=1&btin.doc_no=" + docNo
+                                + "&btin.appl_no=000000&menu_idx=67";
+                    }
+
+                    // 작성자
+                    String author = Optional.ofNullable(row.selectFirst("td.writer"))
+                            .map(Element::text).orElse(null);
+
+                    // 조회수
+                    Integer views = Optional.ofNullable(row.selectFirst("td.hit"))
+                            .map(e -> Integer.parseInt(e.text().trim()))
+                            .orElse(0);
+
+                    // 작성일
+                    String dateString = Optional.ofNullable(row.selectFirst("td.date"))
+                            .map(Element::text).orElse(null);
+
+                    // null check
+                    if (title == null || author == null || dateString == null) {
+                        log.warn("경북대학교 공지사항 크롤링 중 null 데이터가 발견됨");
+                        active = false;
+                        break;
+                    }
+
+                    LocalDate writtenAt = LocalDate.parse(dateString, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+                    if (!checkDateAndSaveArticle(timeWindow, writtenAt, title, link, views, author, boardName,
+                            subName)) {
+                        active = false;
+                        break;
+                    }
+                }
+
+            } catch (IOException e) {
+                throw ArticleExceptionUtils.CrawlingError(url, e.getMessage());
+            }
+        }
     }
 
     private void crawlKNUCSE(int timeWindow) {
@@ -69,32 +177,20 @@ public class ArticleCrawler {
                     String dateString = Optional.ofNullable(row.selectFirst("td.td_datetime"))
                             .map(Element::text).orElse(null);
 
-                    LocalDate writtenAt = null;
-                    if (dateString != null) {
-                        writtenAt = LocalDate.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-                    }
-
-                    // 하루 전보다 오래된 글이면 종료
-                    LocalDate oneDayAgo = LocalDate.now().minusDays(timeWindow);
-                    if (writtenAt != null && writtenAt.isBefore(oneDayAgo)) {
+                    // null check
+                    if (subName == null || title == null || author == null || dateString == null) {
+                        log.warn("경북대학교 컴퓨터학부 공지사항 크롤링 중 null 데이터가 발견됨");
                         active = false;
                         break;
                     }
 
-                    // 최신 글이면 ArticleReqDTO 객체 생성
-                    ArticleReqDTO articleReqDTO = ArticleReqDTO.builder()
-                            .title(title)
-                            .link(link)
-                            .views(views)
-                            .author(author)
-                            .writtenAt(writtenAt)
-                            .boardName(boardName)
-                            .boardSubName(subName)
-                            .build();
+                    LocalDate writtenAt = LocalDate.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-                    log.info("분야: {}, 제목: {}, 작성일: {}", subName, title, writtenAt);
-
-                    articleService.addNotice(articleReqDTO);
+                    if (!checkDateAndSaveArticle(timeWindow, writtenAt, title, link, views, author, boardName,
+                            subName)) {
+                        active = false;
+                        break;
+                    }
                 }
 
             } catch (IOException e) {
