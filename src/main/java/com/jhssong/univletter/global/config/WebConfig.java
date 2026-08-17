83b13ba10821bd5ab4d576cc99fa5477722a1c60
jhssong <jhssong02@gmail.com>
2026-08-17 11:15:27 +0900

    fix: Fix allowedOrigins url typo

--- src/main/java/com/jhssong/univletter/global/config/WebConfig.java
+++ src/main/java/com/jhssong/univletter/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "https://univletter1.jhssong.com")
+                .allowedOrigins("http://localhost:5173", "https://univletter.jhssong.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .allowedHeaders("*");
